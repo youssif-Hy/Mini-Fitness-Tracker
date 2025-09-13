ca323c9abe528e514c25b3f1f38f74a1026eef1b
Youssef Harby <youssif2371@gmail.com>
2025-09-13 23:16:02 +0300

    Add new classes and organize project structure
    Introduced several new classes: `FitnessAppEngine`, `Exercise`, `ProgressTracker`, `User`, `WorkOutPlan`, `ConsoleUI`, and `DataHandler`, each within their respective namespaces. Updated the project file to include new folders for `Engine` and `Models` to enhance code organization.

---
+++ Mini Fitness Tracker/Utils/DataHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mini_Fitness_Tracker.Utils
+{
+    internal class DataHandler
+    {
+    }
+}
